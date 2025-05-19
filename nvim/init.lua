8ec78cd573014dda5e161d052d7975d55787a12f
Miroslav Rachev <miro@mirosr.net>
2025-05-19 15:38:37 +0300

    neovim: Comply with the new `mason-lspconfig` setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -465,53 +465,22 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
-local servers = {
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
-  -- solargraph = {},
-  ts_ls = {},
-  html = {},
-  cssls = {},
-  graphql = {},
-  sqlls = {},
-  marksman = {},
-  eslint = {},
-}
-
-require('neodev').setup()
-
--- nvim-cmp
---- Broadcast the additional completion capabilities to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
 -- Setup mason so it can manage external tooling
 require('mason').setup()
 
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
+require("mason-lspconfig").setup {
+  automatic_enable = {
+    "lua_ls",
+    "solargraph",
+    "ts_ls",
+    "html",
+    "cssls",
+    "graphql",
+    "sqlls",
+    "marksman",
+    "eslint"
+  },
+  ensure_installed = { "lua_ls" }
 }
 
 local cmp = require 'cmp'
